664112a9edd319eb6b448e7691b7f36b946000fe
endlessborland <endlessborland@gmail.com>
2017-12-27 05:37:59 +0300

    Fixed double

--- src/pratwo/DoubleFrame.java
+++ src/pratwo/DoubleFrame.java
@@ -31,6 +31,8 @@ public class DoubleFrame extends BasicFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         table = new javax.swing.JTable();
         start = new javax.swing.JButton();
+        resultField = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
 
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
@@ -65,6 +67,13 @@ public class DoubleFrame extends BasicFrame {
             }
         });
 
+        jButton1.setText("Обратный перевод");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -81,7 +90,12 @@ public class DoubleFrame extends BasicFrame {
                         .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 547, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 547, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(resultField, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -95,7 +109,11 @@ public class DoubleFrame extends BasicFrame {
                     .addComponent(start))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(234, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(resultField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addContainerGap(205, Short.MAX_VALUE))
         );
 
         pack();
@@ -135,10 +153,42 @@ public class DoubleFrame extends BasicFrame {
         this.table.setValueAt(res, 0, 2);
     }//GEN-LAST:event_startActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String number = "";
+        number += this.table.getValueAt(0, 0).toString();
+        String charac = Long.toBinaryString(Long.parseLong(this.table.getValueAt(0, 1).toString()));
+        while (charac.length() < 11)
+            charac = "0" + charac;
+        number += charac;
+        String man = Long.toBinaryString(Long.parseLong(this.table.getValueAt(0, 2).toString()));
+        number += man;
+        //JOptionPane.showMessageDialog(null, number);
+        String a = "";
+        for (int i = 1; i < 12; i++)
+        {
+            a += number.charAt(i);
+        }
+        long _charac = Long.parseLong(a, 2) - 1023;
+        double result = Math.pow(2, _charac);
+        for (int i = 12; i < 64; i++)
+        {
+            _charac--;
+            try {
+            result += ((number.charAt(i) == '0') ? 0 : 1) * Math.pow(2, _charac);
+            } catch (Exception e) {}
+        }
+        result *= number.charAt(0) == '0' ? 1 : -1;
+        this.resultField.setText("" + result);
+
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField inputField;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField resultField;
     private javax.swing.JButton start;
     private javax.swing.JTable table;
     // End of variables declaration//GEN-END:variables
